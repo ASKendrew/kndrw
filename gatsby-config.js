61a046d5dca364f3e5be7262c9203c463fcfaf7e
Adam K <24941578+ASKendrew@users.noreply.github.com>
2020-09-27 00:45:17 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,7 @@ if (process.env.NODE_ENV === `production` && config.siteUrl === `http://localhos
 */
 module.exports = {
     pathPrefix: `/blog`,
+    assetPrefix: `/blog`,
     siteMetadata: {
         siteUrl: process.env.SITEURL || config.siteUrl,
 },

f513d72c1186583915dba2e00bc0ca415d648795
Adam K <24941578+ASKendrew@users.noreply.github.com>
2020-09-27 01:04:45 +0100

    Update siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `https://kndrwghost.netlify.app`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://kndrwghost.netlify.app/blog`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
